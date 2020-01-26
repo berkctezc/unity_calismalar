35092498b71c0be5130ac0cc71ac511de881c233
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2020-01-27 00:17:55 +0300

    player input and relational camera to player object

---
+++ basics/Assets/CS-Scripts/CameraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject characterRef;
+
+    //3 boyutlu vector
+    private Vector3 cam_characterDistance;
+    
+    void Start()
+    {
+        //transform yaratılan GameObject'e erişecek
+        cam_characterDistance = transform.position - characterRef.transform.position;
+    }
+
+    //bütün updatelerden sonra yapılacak update
+    private void LateUpdate()
+    {
+        transform.position = characterRef.transform.position + cam_characterDistance;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ basics/Assets/CS-Scripts/UserMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserMovement : MonoBehaviour
+{
+    public float 
+        forceMultiplier=1;
+    
+    private Rigidbody rigidbodyref;
+    
+    
+    
+    void Start()
+    {
+        rigidbodyref = GetComponent<Rigidbody>();
+    }
+    
+
+    void FixedUpdate()
+    {
+        // user input
+        rigidbodyref
+            .AddForce(
+                Input.GetAxis("Vertical") * forceMultiplier,
+                0f,
+                -Input.GetAxis("Horizontal") * forceMultiplier
+            );
+    }
+}
